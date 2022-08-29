50010ef83ced1c892bc8a86f2bf8d11c3d54f136
Aditya Kumar <ak9001099436@gmail.com>
2022-08-30 00:06:09 +0530

    fix

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,18 @@ import javafx.application.Application;
 import java.io.File;
 
 public class Main extends Application {
-    String dir=System.getProperty("user.home");
+    String dir=System.getProperty("user.dir");
     @Override
     public void start(Stage stage) throws Exception {
-         FXMLLoader loader = new FXMLLoader(new File(dir+"/Music/thermo/main.fxml").toURI().toURL());
+         FXMLLoader loader = new FXMLLoader(new File(dir+"/plotter/ui/main.fxml").toURI().toURL());
+        loader.setController(new homepage());
+        Parent root = loader.load();
+        Stage primaryStage=new Stage();
+        primaryStage.setScene(new Scene(root,1298,760));
+        primaryStage.show();
 
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
